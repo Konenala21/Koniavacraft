bae2c85893dffeac1780ff3be71aa56f4913ced8
果果Konenala21 <notsay@email>
2026-01-05 01:14:34 +0800

    整理礦石研磨機系統

--- src/main/java/com/github/nalamodikk/common/block/blockentity/ore_grinder/OreGrinderScreen.java
+++ src/main/java/com/github/nalamodikk/common/block/blockentity/ore_grinder/OreGrinderScreen.java
@@ -3,26 +3,23 @@ package com.github.nalamodikk.common.block.blockentity.ore_grinder;
 import com.github.nalamodikk.KoniavacraftMod;
 import com.github.nalamodikk.client.screenAPI.component.ArrowProgressWidget;
 import com.github.nalamodikk.client.screenAPI.component.ManaBarWidget;
-import com.github.nalamodikk.client.screenAPI.framework.ModularScreen;
+import com.github.nalamodikk.client.screenAPI.framework.AutoSizedModularScreen;
 import com.github.nalamodikk.client.screenAPI.framework.Panel;
-import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 
 /**
- * ⚙️ 粉碎機 GUI 界面 (模組化版本)
+ * ⚙️ 粉碎機 GUI 界面 (v2 - 自動尺寸)
  */
-public class OreGrinderScreen extends ModularScreen<OreGrinderMenu> {
+public class OreGrinderScreen extends AutoSizedModularScreen<OreGrinderMenu> {
 
-    // GUI 材質位置 (大圖)
     private static final ResourceLocation TEXTURE =
             ResourceLocation.fromNamespaceAndPath(KoniavacraftMod.MOD_ID, "textures/gui/ore_grinder_gui.png");
 
     public OreGrinderScreen(OreGrinderMenu menu, Inventory playerInventory, Component title) {
-        super(menu, playerInventory, title);
-        this.imageWidth = 176;
-        this.imageHeight = 222;
+        // ✨ v2: 自動檢測尺寸並繪製背景
+        super(menu, playerInventory, title, TEXTURE, 176, 222);
     }
 
     @Override
@@ -43,18 +40,5 @@ public class OreGrinderScreen extends ModularScreen<OreGrinderMenu> {
         ));
     }
 
-    @Override
-    protected void renderBg(GuiGraphics graphics, float partialTick, int mouseX, int mouseY) {
-        // 繪製大背景圖 (包含 Slot 框框)
-        graphics.blit(TEXTURE, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight);
-        
-        // 繪製 Widget
-        super.renderBg(graphics, partialTick, mouseX, mouseY);
-    }
-
-    @Override
-    public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
-        super.render(graphics, mouseX, mouseY, partialTick);
-        // 這裡會自動繪製 rootPanel (箭頭與魔力條) 和 Tooltip
-    }
+    // ✨ v2: renderBg 完全刪除，AutoSizedModularScreen 會自動處理
 }
