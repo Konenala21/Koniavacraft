f1a9653174d25b1d27cc790bbd13c098bcac2944
果果Konenala21 <notsay@email>
2026-01-05 01:13:43 +0800

    整理魔力合成台系統

--- src/main/java/com/github/nalamodikk/common/block/blockentity/mana_crafting/ManaCraftingScreen.java
+++ src/main/java/com/github/nalamodikk/common/block/blockentity/mana_crafting/ManaCraftingScreen.java
@@ -1,109 +1,33 @@
 package com.github.nalamodikk.common.block.blockentity.mana_crafting;
 
 import com.github.nalamodikk.KoniavacraftMod;
-import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import com.github.nalamodikk.client.screenAPI.component.ManaBarWidget;
+import com.github.nalamodikk.client.screenAPI.framework.AutoSizedModularScreen;
+import com.github.nalamodikk.client.screenAPI.framework.Panel;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class ManaCraftingScreen extends AbstractContainerScreen<ManaCraftingMenu> {
-    private static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(KoniavacraftMod.MOD_ID, "textures/gui/mana_crafting_table_gui.png");
-    private static final ResourceLocation MANA_BAR_FULL = ResourceLocation.fromNamespaceAndPath(KoniavacraftMod.MOD_ID, "textures/gui/mana_bar_full.png");
-    private static final Logger LOGGER = LogManager.getLogger();
+/**
+ * 🧪 魔力合成台 GUI 界面 (v2 - 自動尺寸)
+ */
+public class ManaCraftingScreen extends AutoSizedModularScreen<ManaCraftingMenu> {
+    private static final ResourceLocation TEXTURE =
+        ResourceLocation.fromNamespaceAndPath(KoniavacraftMod.MOD_ID, "textures/gui/mana_crafting_table_gui.png");
 
     public ManaCraftingScreen(ManaCraftingMenu container, Inventory inv, Component title) {
-        super(container, inv, title);
-        this.imageWidth = 176;  // GUI 界面的宽度
-        this.imageHeight = 166;  // GUI 界面的高度
+        // ✨ v2: 自動檢測尺寸並繪製背景
+        super(container, inv, title, TEXTURE, 176, 166);
     }
 
     @Override
-    protected void renderBg(GuiGraphics guiGraphics, float partialTicks, int mouseX, int mouseY) {
-        // 渲染背景纹理
-
-        guiGraphics.blit(TEXTURE, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight);
-
-
-        // 动态渲染魔力条
-        renderManaBar(guiGraphics, partialTicks);
+    protected void buildGui(Panel root) {
+        // 魔力條 (11, 19) - 7x47
+        root.add(new ManaBarWidget(11, 19,
+            menu::getManaStored,
+            () -> ManaCraftingTableBlockEntity.MAX_MANA
+        ).setSize(7, 47));
     }
 
-    @Override
-    protected void renderLabels(GuiGraphics guiGraphics, int mouseX, int mouseY) {
-        // 渲染屏幕上的文本标签
-        guiGraphics.drawString(this.font, this.title.getString(), this.titleLabelX, this.titleLabelY, 4210752, false);
-        guiGraphics.drawString(this.font, this.playerInventoryTitle.getString(), 8, this.imageHeight - 94, 4210752, false);
-    }
-
-    private void renderManaBar(GuiGraphics guiGraphics, float partialTicks) {
-        // 獲取魔力條的位置
-        int manaBarX = this.leftPos + 11;
-        int manaBarY = this.topPos + 19;
-        int manaBarWidth = 7;
-        int manaBarHeight = 47;
-
-        // 使用 menu 中的 getManaStored() 方法獲取當前的魔力存儲量
-        int manaStored = this.menu.getManaStored();
-        int maxMana = ManaCraftingTableBlockEntity.MAX_MANA;
-        float manaPercentage = (float) manaStored / maxMana;
-        int manaHeight = Math.round(manaPercentage * manaBarHeight);
-
-        // 渲染滿的魔力條部分，使用獨立的滿魔力條圖片
-        if (manaHeight > 0) {
-            // 禁用混合模式來避免透明度問題
-            RenderSystem.disableBlend();
-
-            // 設置紋理顏色為不透明
-            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-
-            // 渲染滿魔力條
-            guiGraphics.blit(MANA_BAR_FULL, manaBarX, manaBarY + (manaBarHeight - manaHeight), 49, 11, manaBarWidth, manaHeight);
-        }
-    }
-
-
-
-    @Override
-    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
-        // 渲染背景和前景
-        this.renderBackground(guiGraphics, mouseX, mouseY, partialTicks);
-        super.render(guiGraphics, mouseX, mouseY, partialTicks);
-        // 渲染工具提示
-        this.renderTooltip(guiGraphics, mouseX, mouseY);
-
-        // 添加鼠标悬停提示
-        renderManaTooltip(guiGraphics, mouseX, mouseY);
-    }
-
-    // 添加一个方法来处理魔力条的鼠标悬停提示
-    private void renderManaTooltip(GuiGraphics guiGraphics, int mouseX, int mouseY) {
-        // 魔力条的位置和大小
-        int manaBarX = this.leftPos + 11;
-        int manaBarY = this.topPos + 19;
-        int manaBarWidth = 7;
-        int manaBarHeight = 47;
-
-        // 检查鼠标是否在魔力条的范围内
-        if (mouseX >= manaBarX && mouseX <= manaBarX + manaBarWidth &&
-                mouseY >= manaBarY && mouseY <= manaBarY + manaBarHeight) {
-
-            // 使用 menu 中的 getManaStored() 方法获取当前的魔力值
-            int manaStored = this.menu.getManaStored();
-            int maxMana = ManaCraftingTableBlockEntity.MAX_MANA;
-
-            // 创建要显示的工具提示内容
-            List<Component> tooltip = new ArrayList<>();
-            tooltip.add(Component.translatable("tooltip.koniava.mana_stored", manaStored, maxMana));
-
-            // 显示工具提示
-            guiGraphics.renderComponentTooltip(this.font, tooltip, mouseX, mouseY);
-        }
-    }
+    // ✨ v2: renderBg 完全刪除，AutoSizedModularScreen 會自動處理
 }
