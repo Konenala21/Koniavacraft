65e065093b15bdd97b662aada0d68c5bdfe9197b
果果Konenala21 <notsay@email>
2026-01-05 21:47:45 +0800

    refactor: limit server payload registration to dedicated

--- src/main/java/com/github/nalamodikk/register/ModNetworkingServer.java
+++ src/main/java/com/github/nalamodikk/register/ModNetworkingServer.java
@@ -6,20 +6,17 @@ import com.github.nalamodikk.common.network.packet.server.rpg.SyncRPGDataPacket;
 import com.github.nalamodikk.narasystem.nara.network.client.NaraSystemIntroMessagePacket;
 import com.github.nalamodikk.narasystem.nara.network.client.OpenNaraInitScreenPacket;
 import com.github.nalamodikk.narasystem.nara.network.server.NaraSyncPacket;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.fml.loading.FMLEnvironment;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 
-@EventBusSubscriber(modid = KoniavacraftMod.MOD_ID)
+@EventBusSubscriber(modid = KoniavacraftMod.MOD_ID, value = Dist.DEDICATED_SERVER)
 public class ModNetworkingServer {
     public static final String VERSION = "1";
 
     @SubscribeEvent
     public static void register(RegisterPayloadHandlersEvent event) {
-        if (FMLEnvironment.dist.isClient()) {
-            return;
-        }
         var registrar = event.registrar("1");
 
         ManaUpdatePacket.registerToServer(registrar);
