e54b365a9235e58bb5e2174c81e4bf8b6c3368c4
果果Konenala21 <notsay@email>
2026-01-08 03:58:33 +0800

    chore: Configure JUnit 5 testing infrastructure

---
+++ src/test/java/com/github/nalamodikk/SimpleUnitTest.java
@@ -0,0 +1,11 @@
+package com.github.nalamodikk;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleUnitTest {
+    @Test
+    void testJUnitWorks() {
+        assertTrue(true, "JUnit 5 should be working!");
+    }
+}
